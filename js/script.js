edac744b0a8acabc94d15fc523f6493b17151426
Winda Sari <sariw3689@gmail.com>
2025-06-18 14:02:44 +0700

    from pencarian

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-// Toggle classs activeAdd commentMore actions
+// Toggle class 'active' untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
+const hamburger = document.querySelector("#hamburger-menu");
 
-// ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+hamburger.onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class 'active' untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
+document .querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox .focus();
+  e.preventDefaul();
+};
+
+// Klik di luar navbar dan hamburger untuk menutup navbar
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
