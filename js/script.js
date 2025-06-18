fbfa735a3bc16ec0631aa2f2a21859d7664687cb
Winda Sari <sariw3689@gmail.com>
2025-06-18 23:19:16 +0700

    menambahkan ikon keranjang

--- js/script.js
+++ js/script.js
@@ -1,50 +1,40 @@
-// Toggle class active
+// Toggle class active pada navbar
 const navbarNav = document.querySelector(".navbar-nav");
-
-// ketika hamburger menu diklik
-const hm = document.querySelector("#hamburger-menu");
-.onclick = () => {
- navbarNav.classList.toggle("active");
-};
-
-// klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
-
+hamburger.onclick = (e) => {
+  e.preventDefault();
+  navbarNav.classList.toggle("active");
+};
 
 // Toggle class active untuk search form
+const searchButton = document.querySelector("#search-button");
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
-document.querySelector("#search-button");
-.onclick = (e) => {
-
+searchButton.onclick = (e) => {
+  e.preventDefault();
   searchForm.classList.toggle("active");
   searchBox.focus();
-  e.preventDefault();
 };
-
-// klik diluar elemen
-const hm = document.querySelector("#hamburger-menu");
-const sb = document.querySelector("#search-button");
-
-document.addEventListener("click" ,function  (e) {
-if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-if (!hm.contains (e.target) && !navbarNav.contains(e.target)) {
-
-navbarNav.classList.remove("active");}
-if (!sb.contains(e.target) &&
-!searchForm.contains(e.target)) {
-searchForm.classList.remove("active");
-}
-});
-
 
 // Toggle class active untuk shopping cart
 const shoppingCart = document.querySelector(".shopping-cart");
-const sc = document.querySelector("#shopping-cart-button");
-
-sc.onclick = (e) => {
-  shoppingCart.classList.toggle("active");
+const cartButton = document.querySelector("#shopping-cart-button");
+cartButton.onclick = (e) => {
   e.preventDefault();
+  shoppingCart.classList.toggle("active");
 };
 
-/
+// Klik di luar elemen untuk menutup menu yang aktif
+document.addEventListener("click", function (e) {
+  if (!navbarNav.contains(e.target) && !hamburger.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+
+  if (!searchForm.contains(e.target) && !searchButton.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  if (!shoppingCart.contains(e.target) && !cartButton.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
