70ce15b37a33b0f1dc71891c227856fbbf475c5a
Winda Sari <sariw3689@gmail.com>
2025-06-18 22:01:01 +0700

    menambahkan daftar produk

--- js/script.js
+++ js/script.js
@@ -1,28 +1,50 @@
-// Toggle class 'active' untuk hamburger menu
+// Toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu diklik
+const hm = document.querySelector("#hamburger-menu");
+.onclick = () => {
+ navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
-hamburger.onclick = () => {
-  navbarNav.classList.toggle("active");
-};
 
-// Toggle class 'active' untuk search form
+// Toggle class active untuk search form
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-button");
+.onclick = (e) => {
 
-document .querySelector('#search-button').onclick = (e) => {
   searchForm.classList.toggle("active");
-  searchBox .focus();
-  e.preventDefaul();
+  searchBox.focus();
+  e.preventDefault();
 };
 
-// Klik di luar navbar dan hamburger untuk menutup navbar
-document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
+// klik diluar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
-  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
-    searchForm.classList.remove("active");
-  }
+document.addEventListener("click" ,function  (e) {
+if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+if (!hm.contains (e.target) && !navbarNav.contains(e.target)) {
+
+navbarNav.classList.remove("active");}
+if (!sb.contains(e.target) &&
+!searchForm.contains(e.target)) {
+searchForm.classList.remove("active");
+}
 });
+
+
+// Toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+const sc = document.querySelector("#shopping-cart-button");
+
+sc.onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+/
