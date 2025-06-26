8b5a23a594dfa83befeeaf63955a3295a7b244ff
Winda Sari <sariw3689@gmail.com>
2025-06-26 14:53:21 +0700

    menambahkan 4.ikon keranjang menampilkan jumlah item

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
 document.addEventListener('alpine:init', () => {
-        Alpine.data('products', () => ({
-           items: [
-            { id: 1, name:'Robusta Brazil', img: '1.jpg', price: 20000},
-            { id: 2, name:'Arabica Blend', img: '2.jpg', price: 25000},
-            { id: 3, name:'Primo Basso', img: '3.jpg', price: 30000},
-            { id: 4, name:'Aceh Gayo', img: '4.jpg', price: 35000},
-            { id: 5, name:'Sumatera Mandheling',img: '5.jpg', price: 40000},
-           ],
+  Alpine.data('products', () => ({
+    items: [
+      { id: 1, name: 'Robusta Brazil', img: '1.jpg', price: 20000 },
+      { id: 2, name: 'Arabica Blend', img: '2.jpg', price: 25000 },
+      { id: 3, name: 'Primo Basso', img: '3.jpg', price: 30000 },
+      { id: 4, name: 'Aceh Gayo', img: '4.jpg', price: 35000 },
+      { id: 5, name: 'Sumatera Mandheling', img: '5.jpg', price: 40000 },
+    ],
+    Rupiah(number) {
+      return new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
+        minimumFractionDigits: 0,
+      }).format(number);
+    }
+  }));
 
-        }));
-    });
+  Alpine.store('cart', {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+        this.items.push(newItem);
+        this.quantity++;
+        this.total+= newItem.price;
+      console.log(this.total);
+    }
+  });
+});
