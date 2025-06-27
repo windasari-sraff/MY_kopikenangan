9397b254b030310d27dd9f1d0010d0eaab1b914b
Winda Sari <sariw3689@gmail.com>
2025-06-27 09:05:50 +0700

    menambahkan 6.ikon keranjang dan harga produk

--- src/app.js
+++ src/app.js
@@ -7,39 +7,43 @@ document.addEventListener('alpine:init', () => {
       { id: 4, name: 'Aceh Gayo', img: '4.jpg', price: 35000 },
       { id: 5, name: 'Sumatera Mandheling', img: '5.jpg', price: 40000 },
     ],
-    Rupiah(number) {
-      return new Intl.NumberFormat('id-ID', {
-        style: 'currency',
-        currency: 'IDR',
-        minimumFractionDigits: 0,
-      }).format(number);
-    }
   }));
+
+  window.rupiah = function(number) {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+      minimumFractionDigits: 0
+    }).format(number);
+  };
 
   Alpine.store('cart', {
     items: [],
-    total: 0,
-    quantity: 0,
-    add(newItem) {
-      const cartItem = this.items.find((item) => item.id === newItem.id);
-
-      if (!cartItem) {
-        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
-        this.quantity++;
-        this.total += newItem.price;
+    add(item) {
+      const found = this.items.find(i => i.id === item.id);
+      if (found) {
+        found.quantity++;
+        found.total = found.quantity * found.price;
       } else {
-        this.items = this.items.map((item) => {
-          if (item.id !== newItem.id) {
-            return item;
-          } else {
-            item.quantity++;
-            item.total = item.price * item.quantity;
-            this.quantity++;
-            this.total += item.price;
-            return item;
-          }
+        this.items.push({
+          ...item,
+          quantity: 1,
+          total: item.price
         });
       }
+    },
+    increase(item) {
+      item.quantity++;
+      item.total = item.price * item.quantity;
+    },
+    decrease(item) {
+      if (item.quantity > 1) {
+        item.quantity--;
+        item.total = item.price * item.quantity;
+      }
+    },
+    get quantity() {
+      return this.items.reduce((acc, i) => acc + i.quantity, 0);
     }
   });
 });
